918b3e483a06f147945fa7d7c081256ad7b6bdaf
Juff-KD <juffcade@gmail.com>
2024-09-05 16:04:43 +0630

    plugin_init modified

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -150,4 +150,25 @@ return {
 		"pluffie/neoproj",
 		cmd = { "ProjectOpen", "ProjectNew" },
 	},
+	{
+		"bgaillard/readonly.nvim",
+		dependencies = {
+			"rcarriga/nvim-notify",
+		},
+		opts = {
+			-- see https://neovim.io/doc/user/lua.html#vim.fs.normalize()
+			secured_files = {
+				"~/%.aws/config",
+				"~/%.aws/credentials",
+				"~/%.ssh/.",
+				"~/%.secrets.yaml",
+				"~/%.vault-crypt-files/.",
+			},
+		},
+		lazy = false,
+	},
+	{
+		"Makaze/watch.nvim",
+		cmd = { "WatchStart", "WatchStop", "WatchFile" },
+	},
 }
